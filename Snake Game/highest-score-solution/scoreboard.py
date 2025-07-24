f205e00a0678ba1ebb03b5ed36b49d1e079097c0
Shanreed25 <shannontlreed@gmail.com>
2025-07-24 03:29:08 -0700

    writing highest score for snake to file

--- Snake Game/highest-score-solution/scoreboard.py
+++ Snake Game/highest-score-solution/scoreboard.py
@@ -24,7 +24,7 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
-            with open("./data.txt", mode="w") as data:
+            with open("data.txt", mode="w") as data:
                 data.write(f"{self.high_score}")
         self.score = 0
         self.update_scoreboard()
